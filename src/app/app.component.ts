093cd7e58327096a427129195fffd3613c6d8fcf
Andrei Ilievici <andrei.ilievici@soft-tehnica.com>
2017-08-30 15:46:14 +0300

    fix path navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(private route: ActivatedRoute, private router: Router) { }
+
+  onAnchorClick() {
+    this.route.fragment.subscribe(f => {
+      const element = document.querySelector("#" + f)
+      if (element)
+        element.scrollIntoView(true);
+      else
+        element.scrollIntoView(false);
+    });
+  }
 }
